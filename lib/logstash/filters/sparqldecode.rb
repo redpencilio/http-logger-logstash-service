defa7187538320f1dd825db48a4e477b7ad217d8
Aad Versteden <madnificent@gmail.com>
2023-12-19 11:08:24 +0100

    Decode sparql request body when we detect it
    Detection is imperfect but it is handy for the cases where it works.
    Adds the SPARQL query readably.

---
+++ lib/logstash/filters/sparqldecode.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+require 'cgi'
+
+class LogStash::Filters::SparqlDecode < LogStash::Filters::Base
+
+  config_name "sparqldecode"
+
+# These values are hardcoded for now because of the need to switch source and dest ip fields based on direction
+#  # The fields where the IP and domain name of a service can be found. Used to fill the IP -> domain name map
+#  config :source_domain_field, :validate => :string, :required => true
+#  config :source_ip_field, :validate => :string, :required => true
+#
+#  # The field containing the IP to be resolved using the map we built
+#  config :dest_ip_field, :validate => :string, :required => true
+#  # The field where the domain name associated with the destination IP will be inserted
+#  config :dest_domain_field, :validate => :string, :required => true
+
+  # Mapping of IPs to Docker service names/domain names
+  attr_accessor :mapping
+
+  public
+  def register
+  end
+
+  public
+  def filter(event)
+    puts "FILTERING IN SPARQLDECODE!"
+
+    if event.get("[http][response][headers][content-type]") == "application/sparql-results+json"
+      # A SPARQL query was sent, let's enrich it'
+      if event.get("[http][request][headers][content-type]") == "application/sparql-query"
+        # POST with content-type sparql-update, get body
+        event.set("[http][request][sparql]", event.get("[http][request][body]"))
+      elsif event.get("[url][query]")
+        query_map = CGI::parse(event.get("[url][query]"))
+        query = query_map["query"] || query_map["update"]
+        event.set("[http][request][sparql]", query) if query
+      end
+    end
+
+    filter_matched(event)
+  end
+end
