8cc70f9244f1a8122fce706192915da1a364f04a
Sergio Fenoll <sergio@fenoll.be>
2024-10-23 14:57:33 +0200

    chore: deduplicate sparqldecode functionality
    The logic to decode SPARQL queries was implemented in the two filters
    defined in this repo. To make things clearer, we remove the sparqldecode
    logic from the ipmap filter and solely implement it in its own filter.
    This filter needs to be added to the app-http-logger config, but it will
    make the implementation of this repo a lot easier to follow.

--- lib/logstash/filters/sparqldecode.rb
+++ lib/logstash/filters/sparqldecode.rb
@@ -13,17 +13,23 @@ class LogStash::Filters::SparqlDecode < LogStash::Filters::Base
 
   public
   def filter(event)
-    if event.get("[http][response][headers][content-type]") == "application/sparql-results+json"
-      # A SPARQL query was sent, let's enrich it'
-      if event.get("[http][request][headers][content-type]") == "application/sparql-query"
-        # POST with content-type sparql-update, get body
-        event.set("[http][request][sparql]", event.get("[http][request][body]"))
-      elsif event.get("[url][query]")
-        query_map = CGI::parse(event.get("[url][query]"))
-        query_map.default = nil
-        query = query_map["query"] || query_map["update"]
-        event.set("[http][request][sparql]", query) if query
+    begin
+      response_content_types = event.get("[http][response][headers][content-type]")
+      request_content_types = event.get("[http][request][headers][content-type]")
+      if response_content_types == "application/sparql-results+json"
+        # A SPARQL query was sent, let's enrich it'
+        if request_content_types == "application/sparql-query"
+          # POST with content-type sparql-update, get body
+          event.set("[http][request][sparql]", event.get("[http][request][body]"))
+        elsif event.get("[url][query]")
+          query_map = CGI::parse(event.get("[url][query]"))
+          query_map.default = nil
+          query = query_map["query"] || query_map["update"]
+          event.set("[http][request][sparql]", query) if query.first && query.first
+        end
       end
+    rescue
+      @logger.warn("Failed to process SPARQL query for #{event}")
     end
 
     filter_matched(event)
